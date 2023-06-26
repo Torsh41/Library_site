5e4dbdc73ae7c4655954a6997730c44c19094825
Leo Kazantsev <leokazantsev19@mail.ru>
2023-06-26 10:43:10 +0500

    preparing to merge

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 import os
 from app.begin_to_app import create_app
-#from flask_script import Manager
-#manager = Manager(app)
+
+
 application = create_app(os.getenv('FLASK_CONFIG') or 'default')
 if __name__ == '__main__':    
     application.run(debug=True)
