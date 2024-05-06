799e8488041efbc716f7bdb88a7c6decbc7b07cc
Leo Kazantsev <leokazantsev19@mail.ru>
2024-05-06 20:48:20 +0300

    bugs fix

--- app/static/js/book.js
+++ app/static/js/book.js
@@ -1,10 +1,8 @@
 let count = document.getElementsByClassName("list__book").length;
 let count1 = document.getElementsByClassName("list__book");
-// console.log(count)
 if (count > 0) {
     console.log(count)
     let addr = document.getElementsByClassName("list__add");
-    // console.log(add)
     for (let value of addr) {
         value.classList.add("elemleft");
     }
@@ -19,16 +17,7 @@ if (count > 0) {
 
 else {
     let addr = document.getElementsByClassName("list__add");
-    // console.log(add)
     for (let value of addr) {
         value.classList.remove("elemleft");
     }
 }
-
-
-// let markOne = document.getElementById("firstMark");
-// let markTwo = document.getElementById("secondMark");
-
-// markTwo.addEventListener("click", function() {
-//     markOne.classList.add("bookChange");
-// });
