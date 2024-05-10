8e9bc47194337a91706e608f56864fedba477f0b
Leo Kazantsev <leokazantsev19@mail.ru>
2024-05-10 21:21:50 +0300

    deploy

--- manage.py
+++ manage.py
@@ -1,12 +1,9 @@
-# import mimetypes
-# mimetypes.add_type('text/javascript', '.js', True)
-# mimetypes.add_type('text/css', '.css')
 import os
 from app import create_app, socketio
 
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+app = create_app(os.getenv('FLASK_CONFIG') or 'production')
 if __name__ == '__main__':    
-    socketio.run(debug=True, app=app)
+    socketio.run(debug=False, app=app)
     
  
