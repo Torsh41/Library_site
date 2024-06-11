361324f5b78827c9fb5174c8ff68ecc70a14eae7
Leo Kazantsev <leokazantsev19@mail.ru>
2024-06-11 10:48:36 +0300

    more symbols for book adding

--- app/personal/forms.py
+++ app/personal/forms.py
@@ -22,13 +22,13 @@ def validate_bookname(form, field):
 class AddNewBookForm(FlaskForm):
     isbn = StringField('BookISBN', validators=[DataRequired('Поле не должно быть пустым.'), Length(1, 64), Regexp('[0-9-]', 0,
     'Название ISBN должно содержать только цифры и тире.')])
-    name = StringField('BookName', validators=[DataRequired('Поле не должно быть пустым.'), Length(1, 64), Regexp('[A-Za-zА-Яа-яЁё ]', 0,
+    name = StringField('BookName', validators=[DataRequired('Поле не должно быть пустым.'), Length(1, 128), Regexp('[A-Za-zА-Яа-яЁё ]', 0,
     'Название книги должно содержать только буквы и пробелы.'), validate_bookname])
-    author = StringField('AuthorName', validators=[DataRequired('Поле не должно быть пустым.'), Length(1, 64), Regexp('[A-Za-zА-Яа-яЁё ]', 0,
+    author = StringField('AuthorName', validators=[DataRequired('Поле не должно быть пустым.'), Length(1, 128), Regexp('[A-Za-zА-Яа-яЁё ]', 0,
     'Имя автора должно содержать только буквы и пробелы.')])
     publishing_house = StringField('HouseName', validators=[DataRequired('Поле не должно быть пустым.'), Regexp('[A-Za-zА-Яа-яЁё ]', 0,
     'Название издательсва должно содержать только буквы и пробелы.'), Length(1, 64)])
-    description = TextAreaField('Description', validators=[DataRequired('Поле не должно быть пустым.'), Length(1, 512)])
+    description = TextAreaField('Description', validators=[DataRequired('Поле не должно быть пустым.'), Length(1, 1024)])
     release_date = DateField('ReleaseDate', validators=[DataRequired('Поле не должно быть пустым.')])
     chapters_count = IntegerField('ChaptersCount', validators=[DataRequired('Поле не должно быть пустым.'), NumberRange(2, 100, message='Здесь невозможно ошибиться:)')])
     submit = SubmitField('Добавить книгу в базу')
